f9d7e5a086eae588a7d5adda1f8223773ab43e5a
Krzysztof Szustkiewicz <szustkiewicz.k@gmail.com>
2018-05-08 14:22:03 +0200

    Added option to move right/left

--- funkcje_ruchu.py
+++ funkcje_ruchu.py
@@ -1,20 +1,11 @@
-for i in range(0, 4):
-    for k in range(0, 3):
-        if macierz[i][k] == macierz[i][k + 1] and macierz[i][k] != 0:
-            macierz[i][k] = 2 * macierz[i][k]
-            macierz[i][k + 1] = 0
-for i in range(0, 4):
-    macierz[i] = list(filter(lambda x: x > 0, macierz[i]))
-    while len(macierz[i]) < 4:
-        macierz[i].append(0)
-
-
-def columns_addition_left(matrix, size):
+def columns_addition_left(matrix, size, direction):
     for row in range(0, size):
         for column in range(0, size - 1):
-            r = row
-            c = column
-            if matrix[r][c] == matrix[r][c + 1] and matrix[r][c] != 0:
-                matrix[r][c] = 2 * matrix[r][c]
-                matrix[r][c + 1] = 0
-
+            if direction is 'left':
+                if matrix[row][column] == matrix[row][column + 1] and matrix[row][column] != 0:
+                   matrix[row][column] = 2 * matrix[row][column]
+                   matrix[row][column + 1] = 0
+            elif direction is 'right':
+                if matrix[row][3 - column] == matrix[row][2 - column] and matrix[row][3 - column] != 0:
+                    matrix[row][3 - column] = 2 * matrix[row][3 - column]
+                    matrix[row][2 - column] = 0
