34d1bd0e7b294e662f3e7e2bced815053c3966fd
Krzysztof Szustkiewicz <szustkiewicz.k@gmail.com>
2018-05-09 11:12:11 +0200

    splitted all direction logic into 1 function ver.1

--- funkcje_ruchu.py
+++ funkcje_ruchu.py
@@ -1,3 +1,5 @@
+from math import fabs
+
 def columns_addition_left(matrix, size, direction):
     for row in range(0, size):
         for column in range(0, size - 1):
@@ -9,3 +11,37 @@ def columns_addition_left(matrix, size, direction):
                 if matrix[row][3 - column] == matrix[row][2 - column] and matrix[row][3 - column] != 0:
                     matrix[row][3 - column] = 2 * matrix[row][3 - column]
                     matrix[row][2 - column] = 0
+
+
+
+#prawo
+if matrix[row][3 - column] == matrix[row][2 - column] and matrix[row][3 - column] != 0:
+    matrix[row][3 - column] = 2 * matrix[row][3 - column]
+    matrix[row][2 - column] = 0
+
+#lewo
+if matrix[row][column] == matrix[row][column + 1] and matrix[row][column] != 0:
+    matrix[row][column] = 2 * matrix[row][column]
+    matrix[row][column + 1] = 0
+
+#gora
+
+if macierz[i][k] == macierz[i + 1][k] and macierz[i][k] != 0:
+    macierz[i][k] = 2 * macierz[i][k]
+    macierz[i + 1][k] = 0
+
+#dol
+
+if macierz[3 - i][k] == macierz[2 - i][k] and macierz[3 - i][k] != 0:
+    macierz[3 - i][k] = 2 * macierz[3 - i][k]
+    macierz[2 - i][k] = 0
+
+def comparison_and_addition(a, b, c, d):
+    if matrix[fabs(row +a)][fabs(column + b)] != 0:
+        if matrix[fabs(row + a)][fabs(column + b)] == matrix[fabs(row + c)][fabs(column + d)]:
+            matrix[fabs(row + a)][fabs(column + b)] = 2 * matrix[fabs(row +a)][fabs(column + b)]
+            matrix[fabs(row + c)][fabs(column + d)] = 0
+
+
+
+
