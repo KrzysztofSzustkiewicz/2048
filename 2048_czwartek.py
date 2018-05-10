5b40ec77a885ebc5e3fbb86f4978c24675b87fdc
adamcichecki <a.cichecki@gmail.com>
2018-05-10 15:02:54 +0200

    Nowe

--- 2048_czwartek.py
+++ 2048_czwartek.py
@@ -2,47 +2,70 @@ import os
 import random
 import getch
 
+
 from math import fabs
 from termcolor import colored
+
 
 color_key = {0: 'white',
              2: 'red',
              4: 'green',
              8: 'cyan',
              16: 'yellow',
-             32: 'magenta',
+             32: 'red',
              64: 'green',
              128: 'cyan',
              256: 'yellow',
-             512: 'magenta',
+             512: 'red',
              1024: 'green',
              2048: 'blue'
              }
 
-def elements_movement(size, matrix, reversion=None):
+
+def elements_in_row_movement(size, matrix, reversion=None):
     for index in range(0, size):
-        if reversion is 'yes':
+        if reversion is 'reversed':
             matrix[index] = list(reversed(matrix[index]))
         matrix[index] = list(filter(lambda x: x > 0, matrix[index]))
         while len(matrix[index]) < size:
             matrix[index].append(0)
-        if reversion is 'yes':
+        if reversion is 'reversed':
             matrix[index] = list(reversed(matrix[index]))
+
+def elements_in_column_movement(size, matrix, reversion=None):
+    for column in range(0, size):
+        work_list = []
+        j = 0
+        for row in range(0, size):
+            work_list.append(matrix[row][column])
+        if reversion is 'reversed':
+            work_list = list(reversed(work_list))
+        work_list = list(filter(lambda x: x > 0, work_list))
+        while len(work_list) < size:
+            work_list.append(0)
+        if reversion is 'reversed':
+            work_list = list(reversed(work_list))
+        for row in range(0, size):
+            matrix[row][column] = work_list[j]
+            j += 1
+
 
 def puts_random_2(times):
     random_1 = random.randrange(0, 4)
     random_2 = random.randrange(0, 4)
+    random_num = random.randrange(2,5,2)
     for option in range(times):
-
-        if array[random_1][random_2] == 0:
-            array[random_1][random_2] = 2
-        else:
+         if array[random_1][random_2] == 0:
+            array[random_1][random_2] = random_num
+         else:
             puts_random_2(1)
+
 
 def search_for_zero():
     for row in array:
         if 0 in row:
             return 'lose'
+
 
 def search_win_con(win_con):
     for row in array:
@@ -61,6 +84,7 @@ def print_line(x, y, z, i):
 
 def print_array():
     clear()
+    print(' ' * 5, 'Your score is %d' % score_game)
     for i in range(0, len(array)):
         print_line(0, 1, 1, i)
         print_line(1, 2, 0, i)
@@ -106,6 +130,12 @@ def matrix_index_dictionaries_creation(size):
             'index_f': size,
             }
 
+score_game = 0
+
+def score_of_game(score_1):
+    global score_game
+    score_game += score_1
+
 
 def matrix_addition(matrix, direction):
     a = direction['index_a']
@@ -121,6 +151,7 @@ def matrix_addition(matrix, direction):
                 int(fabs(column + d))]:
                 matrix[int(fabs(row + a))][int(fabs(column + b))] = 2 * matrix[int(fabs(row + a))][
                     int(fabs(column + b))]
+                score_of_game(matrix[int(fabs(row + a))][int(fabs(column + b))])
                 matrix[int(fabs(row + c))][int(fabs(column + d))] = 0
 
 
@@ -135,61 +166,43 @@ matrix_index_dictionaries_creation(size)
 
 print_array()
 
+
 while search_win_con(2048) is not 'win' and search_for_zero() is 'lose':
 
-    print("\n\n", " "*2, "Operate with W S A D", "\n Your goal is to score 2048!")
+    print('\n\n', ' ' * 2, 'Operate with W S A D', '\n Your goal is to score 2048!')
     user_input = getch.getch()
 
-    if user_input == "a":
+
+    if user_input == 'a':
+        elements_in_row_movement(size, array)
         matrix_addition(array, left)
-        elements_movement(size,array)
+        elements_in_row_movement(size, array)
         puts_random_2(1)
-
-    elif user_input == "d":
+    elif user_input == 'd':
+        elements_in_row_movement(size, array, 'reversed')
         matrix_addition(array, right)
-        elements_movement(size, array,'yes')
+        elements_in_row_movement(size, array, 'reversed')
         puts_random_2(1)
 
-    elif user_input == "\x1b[A":
+    elif user_input == 'w':
+        elements_in_column_movement(size, array)
         matrix_addition(array, up)
-        list_1 = []
-        j = 0
-        for k in range(0, 4):
-            for i in range(0, 4):
-                list_1.append(array[i][k])
-            list_1 = list(filter(lambda x: x > 0, list_1))
-            while len(list_1) < 4:
-                list_1.append(0)
-            for i in range(0, 4):
-                array[i][k] = list_1[j]
-                j += 1
-            j = 0
-            list_1 = []
+        elements_in_column_movement(size, array)
         puts_random_2(1)
 
-    elif user_input == "s":
+    elif user_input == 's':
+        elements_in_column_movement(size, array, 'reversed')
         matrix_addition(array, down)
-        list_1 = []
-        j = 3
-        for k in range(0, 4):
-            for i in range(0, 4):
-                list_1.append(array[3 - i][k])
-            list_1 = list(filter(lambda x: x > 0, list_1))
-            while len(list_1) < 4:
-                list_1.append(0)
-            for i in range(0, 4):
-                array[i][k] = list_1[j]
-                j -= 1
-            j = 3
-            list_1 = []
+        elements_in_column_movement(size, array, 'reversed')
         puts_random_2(1)
+
     print_array()
+
 
 if search_win_con(2048) is 'win':
-    clear()
     print_array()
-    print("\n"*2, " "*5, "YOU WON THE GAME!")
+    print('\n'*2, ' '*5, 'YOU WON THE GAME!')
+
 else:
-    clear()
     print_array()
-    print("\n "*2, " "*6, "GAME OVER!", "\n", " "*5, "NO MORE MOVES!\n")
+    print('\n ' * 2, ' ' * 6, 'GAME OVER!', '\n', ' ' * 5, 'NO MORE MOVES!\n')
